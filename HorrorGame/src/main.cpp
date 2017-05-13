34ae4eb961955649c09330c3fb96e5f1051ff107
XMaster <XMaster96@gmx.de>
2017-05-13 21:33:29 +0200

    Neue Fragen

--- HorrorGame/src/main.cpp
+++ HorrorGame/src/main.cpp
@@ -18,14 +18,15 @@ int main()
 {
 
 	sf::RenderWindow window(sf::VideoMode(720, 720), "Horror Game");
-	sf::Texture monitor_texture, button_texture, message1_texture, message2_texture, message3_texture;
+	sf::Texture monitor_texture, button_texture, message1_texture, message2_texture, message3_texture, message4_texture;
 	monitor_texture.loadFromFile("./res/Monitor.png");
 	button_texture.loadFromFile("./res/Button.png");
 	message1_texture.loadFromFile("./res/Message_1.png");
 	message2_texture.loadFromFile("./res/Message_2.png");
 	message3_texture.loadFromFile("./res/Message_3.png");
+	message4_texture.loadFromFile("./res/Message_4.png");
 
-	sf::Sprite monitor, button, message1, message2, message3;
+	sf::Sprite monitor, button, message1, message2, message3, message4;
 	monitor.setTexture(monitor_texture);
 	monitor.setPosition(0.f, 0.f);
 
@@ -38,13 +39,14 @@ int main()
 
 	message2.setTexture(message2_texture);
 	message3.setTexture(message3_texture);
+	message4.setTexture(message4_texture);
 
 	bool button_pressed = false;
 	int menu_status = 0;
 
 	while (window.isOpen())
 	{
-		if (menu_status <= 2)
+		if (menu_status <= 3)
 			window.clear(sf::Color(25, 25, 25));
 		else
 			window.clear(sf::Color(RGBGenerator(), RGBGenerator(), RGBGenerator(), 255));
@@ -59,6 +61,9 @@ int main()
 
 		else if (menu_status == 2)
 			window.draw(message3);
+
+		else if (menu_status == 3)
+			window.draw(message4);
 
 		else
 		{
@@ -79,7 +84,7 @@ int main()
 			else if (event.type == sf::Event::MouseButtonPressed)
 			{
 
-				if (menu_status <= 2)
+				if (menu_status <= 3)
 					menu_status++;
 				else
 				{
