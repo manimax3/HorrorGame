ac9b803a3e9c588d4446b8aa9cb4956e04797b01
manimax3 <manimax3@hotmail.de>
2017-05-13 21:34:34 +0200

    varibale anzahl messages

--- HorrorGame/src/main.cpp
+++ HorrorGame/src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <ctime>
 #include <random>
+#include <array>
 
 #include "SFML/Graphics.hpp"
 
@@ -14,18 +15,25 @@ int RGBGenerator()
 }
 
 
+constexpr unsigned int NUMBER_MENUES = 3;
+
+
 int main()
 {
 
 	sf::RenderWindow window(sf::VideoMode(720, 720), "Horror Game");
-	sf::Texture monitor_texture, button_texture, message1_texture, message2_texture, message3_texture;
+
+	sf::Texture monitor_texture, button_texture;
+	std::array<sf::Texture, NUMBER_MENUES> message_textures;
+
+
 	monitor_texture.loadFromFile("./res/Monitor.png");
 	button_texture.loadFromFile("./res/Button.png");
-	message1_texture.loadFromFile("./res/Message_1.png");
-	message2_texture.loadFromFile("./res/Message_2.png");
-	message3_texture.loadFromFile("./res/Message_3.png");
 
-	sf::Sprite monitor, button, message1, message2, message3;
+	for (int i = 0; i < message_textures.size(); ++i)
+		message_textures[i].loadFromFile(std::string("./res/Message_") + std::to_string(i + 1) + ".png");
+
+	sf::Sprite monitor, button;
 	monitor.setTexture(monitor_texture);
 	monitor.setPosition(0.f, 0.f);
 
@@ -33,32 +41,25 @@ int main()
 	button.setOrigin(sf::Vector2f(0.f, button.getLocalBounds().height));
 	button.setPosition(20.f, window.getSize().y - 20);
 
-	message1.setTexture(message1_texture);
-	message1.setPosition(0, 0);
+	std::array<sf::Sprite, NUMBER_MENUES> messages;
 
-	message2.setTexture(message2_texture);
-	message3.setTexture(message3_texture);
+	for (int i = 0; i < messages.size(); ++i)
+		messages[i].setTexture(message_textures[i]);
 
 	bool button_pressed = false;
 	int menu_status = 0;
 
 	while (window.isOpen())
 	{
-		if (menu_status <= 2)
+		if (menu_status <= NUMBER_MENUES - 1)
 			window.clear(sf::Color(25, 25, 25));
 		else
 			window.clear(sf::Color(RGBGenerator(), RGBGenerator(), RGBGenerator(), 255));
 
 		window.draw(monitor);
 
-		if (menu_status == 0)
-			window.draw(message1);
-
-		else if (menu_status == 1)
-			window.draw(message2);
-
-		else if (menu_status == 2)
-			window.draw(message3);
+		if (menu_status <= NUMBER_MENUES - 1)
+			window.draw(messages[menu_status]);
 
 		else
 		{
@@ -79,7 +80,7 @@ int main()
 			else if (event.type == sf::Event::MouseButtonPressed)
 			{
 
-				if (menu_status <= 2)
+				if (menu_status <= NUMBER_MENUES - 1)
 					menu_status++;
 				else
 				{
